c92f117a2b51104adc925727044ab5dda5999d75
Rong <st9896536@gmail.com>
2018-05-15 21:44:04 +0800

    線上筆試

---
+++ 0515_Q1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 15 21:03:27 2018
+
+@author: Rong
+"""
+
+s = input("input:")
+result = ""  #回傳的字串
+print(s[::-1])
---
+++ 0515_Q2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 15 21:09:06 2018
+
+@author: Rong
+"""
+
+num = int(input("input a number:"))
+i = 1
+ans = []
+for i in range(num):
+    if i % 3 != 0 and i % 5 != 0 :
+        ans.append(i)
+    elif i % 3 == 0 and i % 5 == 0:
+        ans.append(i)
+print(ans)
